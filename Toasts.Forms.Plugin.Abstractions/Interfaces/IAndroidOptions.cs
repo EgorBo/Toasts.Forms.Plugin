493360e99bc43212d5039f679b9ad86b94677478
Adam Pedley <adam.pedley@gmail.com>
2016-09-29 11:11:50 +1000

    Missing AndroidOptions added to interface

--- Toasts.Forms.Plugin.Abstractions/Interfaces/IAndroidOptions.cs
+++ Toasts.Forms.Plugin.Abstractions/Interfaces/IAndroidOptions.cs
@@ -6,5 +6,7 @@
         /// Applicable only to Notification.Builder, if you want to replace the small icon, you must place an image in your drawables folder and pass the int through here. e.g. Resources.Drawable.MyNewIcon
         /// </summary>
         int? SmallDrawableIcon { get; set; } 
+        string ViewText { get; set; }
+        string DismissText { get; set; }
     }
 }
